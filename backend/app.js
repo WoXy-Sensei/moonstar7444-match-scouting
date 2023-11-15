4a4325e11d4f4517d86250decc8a19d17a372d55
WoXy-Sensei <bardiagamer8600@gmail.com>
2023-11-15 12:51:25 +0300

    Update backend files: add .vercel to .gitignore, update app.js to use require instead of import, and modify vercel.json for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,11 @@
-import express from "express";
-import mongoose from "mongoose";
-import Robot from "./models/robot.model.js";
-import cors from "cors";
-import bodyParser from "body-parser";
+const PORT = process.env.PORT || 3000
+const express = require('express')
+const app = express()
 
-const app = express();
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-  
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}.`)
+})
 
-
-app.get("/api", (req, res) => {
-  res.send("Pit Scouting Server");
-});
-
-app.post("/api/team", async (req, res) => {
-  const newRobot = Robot.Robot(req.body);
-  const insertedRobot = await newRobot.save();
-  return res.status(201).json(insertedRobot);
-});
-
-
-const start = async () => {
-  try {
-    mongoose.set("strictQuery", false);
-    await mongoose.connect(
-      "mongodb+srv://bardiaramez:bardia1386@spotify.bg1xthc.mongodb.net/teamscout?retryWrites=true&w=majority"
-    );
-    app.listen(3000, () => console.log("Server started on port 3000"));
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-start();
+app.get('/', function (req, res) {
+  res.send(`Hello ${process.env.HELLO}`)
+})
